eb935fb73719fb9758e64d27452e35c0cf409f80
Dasun <sachindradasun@gmail.com>
2021-01-12 23:58:33 +0800

    Add comment

--- src/main/java/example/dbschemagenerator/DbSchemaGeneratorApplication.java
+++ src/main/java/example/dbschemagenerator/DbSchemaGeneratorApplication.java
@@ -3,6 +3,9 @@ package example.dbschemagenerator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Database schema generator application.
+ */
 @SpringBootApplication
 public class DbSchemaGeneratorApplication {
 
